54b452156a12d41d38e42f910c9520df39680dbf
Arne Halvorsen <arha@dips.no>
2023-04-10 13:18:36 +0200

    wip: changes to demo project as part of readme work

--- src/Fit.Demo.Test/ToDoAndTagCase.cs
+++ src/Fit.Demo.Test/ToDoAndTagCase.cs
@@ -4,22 +4,16 @@ using Fit.Demo.Test.Actor;
 
 namespace Fit.Demo.Test;
 
-public class CaseDefiner : ICaseDefiner
+public class ToDoAndTagCase : ICaseDefiner
 {
     public void AddCases(IFit fit)
     {
-        fit.First("Login").With("Name", "Arne").AsSegment("Login");
-        fit.First("SaveWork").Then("Logout").AsSegment("Exit");
-
-        fit.FirstDo("Login")
-            .Then<AddToDo>().With("Name", "TestToDoItem1").And("State", "Next")
+        fit.First<AddToDo>().With("Name", "TestToDoItem1").And("State", "Next")
             .Then<AddToDo>().With("Name", "TestToDoItem2").And("State", "SAP")
             .Then<AddTag>().With("Name", "Tag1")
             .Then<AddTag>().With("Name", "Tag2")
             .Then("RemoveTag").With("Name", "Tag1")
-            .ThenContinueWith("Exit")
-            .ThenContinueWith("Login")
-            .AsCase("FirstCase");
+            .AsCase("ToDoAndTag");
     }
 
 }
