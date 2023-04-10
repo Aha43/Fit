f3bfffc0ec2674ddda2f19f4bbb4016512512b7e
Arne Halvorsen <arha@dips.no>
2023-04-10 15:25:47 +0200

    wip: added /// comments on meaning of properties

---
+++ src/Demo/Fit.Demo.Test/DemoTest_Method1.cs
@@ -0,0 +1,24 @@
+﻿using Fit.Abstraction.Api;
+using Fit.Demo.Business;
+using Fit.Demo.Infrastructure.InMemory;
+
+namespace Fit.Demo.Test;
+
+public class DemoTest_Method1
+{
+    private readonly IFit _fit = FluentIntegrationTest.Create(o =>
+    {
+        o.RunMode.Proto = true;
+        o.RunMode.IgnoreMissingActors = true;
+        o.Services.AddFitDemoInMemoryInfrastructure()
+            .AddFitDemoBusiness();
+    });
+
+    [Theory]
+    [InlineData("ToDoAndTag")]
+    public async Task Test(string caseName)
+    {
+        await _fit.RunCase(caseName);
+    }
+
+}
--- src/Fit/Implementation/RunMode.cs
+++ src/Fit/Implementation/RunMode.cs
@@ -4,6 +4,13 @@ namespace Fit.Implementation;
 
 internal class RunMode : IRunMode
 {
+    /// <summary>
+    /// If true will not throw exception if actor class not found, usefull for tdd strategy. Default is false.
+    /// </summary>
     public bool IgnoreMissingActors { get; set; } = false;
+
+    /// <summary>
+    /// If true will not actually act (run actors). Default is false.
+    /// </summary>
     public bool Proto { get; set; } = false;
 }
